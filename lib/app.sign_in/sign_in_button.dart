125c008b9e3495069aa118a63449722cf69f61a7
rumancha <rumancha12@gmail.com>
2020-05-03 00:55:23 +0545

    initialize app

---
+++ lib/app.sign_in/sign_in_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../common_widget/custom_raised_button.dart';
+
+class SignInButton extends CustomRaisedButton {
+  SignInButton({
+    @required String text,
+    Color color,
+    Color textColor,
+    VoidCallback onPressed,
+  }) : super(
+          child: Text(
+            text,
+            style: TextStyle(color: textColor, fontSize: 15.0),
+          ),
+          color: color,
+          height: 40.0,
+          onPressed: onPressed,
+        );
+}
---
+++ lib/app.sign_in/sign_in_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import '../common_widget/custom_raised_button.dart';
+import './sign_in_button.dart';
+import './social_sign_in_button.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 48.0),
+          SocialSignInButton(
+            assetName: 'images/google-logo.png',
+            text: 'Sign in with Google',
+            color: Colors.white,
+            textColor: Colors.black87,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SocialSignInButton(
+            assetName: 'images/facebook-logo.png',
+            text: 'Sign in with Facebook',
+            color: Color(0xFF334D92),
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with email',
+            color: Colors.teal[700],
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go annonymous',
+            color: Colors.lime,
+            textColor: Colors.black,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
